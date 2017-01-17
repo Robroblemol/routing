927ff6681352344e2be6ab644f6f496daa94f6ae
Robroblemol <robertodavidrobles@gmail.com>
2017-01-17 19:34:17 +0000

    se agrega otra ruta

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var app = express();// inicialixamos el objeto express
 
 app.set("view engine","jade");
 
+//Verbos Http => GET / POST /PUT/ PATCH/ HEADER /DELETE
+
 app.get("/",function(req,res) {
   res.render("index");
   //res.send("hola mundo con express!!");// invia la respuesta y cierra la conexion
 });
 app.post("/",function (req,res) {
   res.render("form");
+});
+app.get("/algo",function (res,req) {
+    res.render("form");
 });
 
 app.listen(8080);// escuchamos peticiones
